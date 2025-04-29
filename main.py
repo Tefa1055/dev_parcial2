67bbaa3adca6d435627a2b5c2c1e1d01af81d591
labsis <mechaparro81@ucatolica.edu.co>
2025-04-29 05:03:48 -0500

    class

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from contextlib import asynccontextmanager
+from enum import unique
+from lib2to3.pytree import Base
+
 from fastapi import FastAPI
 from utils.connection_db import init_db
-from sqlalchemy.sql import column, integer, string, boolean, DateTime, ForeignKey, enum
+from sqlalchemy import column, integer, string, boolean, DateTime, ForeignKey, enum
 
-
+class Usuario(Base):
+    __tablename__ = "usuario"
+    id = Column(integer, primary_key=True, index=True)
+    nombre = Column(string(50), index=True)
+    email = Column(string(100), unique=True, index=True)
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     await init_db()
